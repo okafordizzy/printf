4b4fe0fb5281cacfb9aa3ca8f3435d0b4196c29c
Dozay <edoziechidili@gmail.com>
2022-07-14 11:41:12 +0100

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -1,45 +1,41 @@
 #include "main.h"
 
-char *_itoa(int num)
+/**
+ * get_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: Precision.
+ */
+int get_precision(const char *format, int *i, va_list list)
 {
-	char *str;
-	int i = 1, m, k = 0, len, power, keep;
-	len = num < 0 ? 3 : 2;
-	k = num < 0 ? 1 : 0;
-	num *= num < 0 ? -1 : 1;
+	int curr_i = *i + 1;
+	int precision = -1;
 
-	keep = num;
-	
+	if (format[curr_i] != '.')
+		return (precision);
 
-	while (keep > 9)
+	precision = 0;
+
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
-		keep /= 10;
-		len++;
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
 	}
 
-	power = len - 2;
+	*i = curr_i - 1;
 
-	str = malloc(sizeof(char) * len);
-	if (str == NULL)
-	{
-		exit (1);
-	}
-	else if (k == 1)
-	{
-		str[0] = '-';
-		power -= 1;
-	}
-
-	for (m = 0; m < power; m++)
-	{
-		i *= 10;
-	}
-	while (i >= 1)
-	{
-		str[k] = ((num / i) % 10) + '0';
-		i /= 10;
-		k++;
-	}
-	str[k] = '\0';
-	return (str);
+	return (precision);
 }
